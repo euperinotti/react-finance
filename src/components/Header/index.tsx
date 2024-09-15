95ee9b21ca4164f9eeb02acade8697ae33b65e4d
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-14 22:40:55 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="w-full h-60 flex items-center justify-center bg-slate-300">
+      <div className="max-w-safe-viewport">
+        <h1 className="text-4xl font-bold">Next.js</h1>
+      </div>
+    </header>
+  );
+};
