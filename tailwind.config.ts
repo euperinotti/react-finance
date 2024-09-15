f2294faf25ae4045da7172651eef74b1a41de14e
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-14 22:42:10 -0300

    feat: add custom max width attribute

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      maxWidth: {
+        "safe-viewport": "1200px",
+      },
     },
   },
   plugins: [],
