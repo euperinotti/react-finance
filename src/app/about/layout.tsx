b3c47b8fd8255f76919420a79f4e8e42df5d49ae
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-18 21:58:50 -0300

    feat: add about page

---
+++ src/app/about/layout.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "React finance",
+  description: "About the creator",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased w-full min-h-full h-dvh`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import { Footer } from "@/components/Footer";
+
+export default function About() {
+  return (
+    <div className="w-full flex flex-col items-center justify-center">
+      <main className="w-full flex flex-col items-center justify-center gap-6 -mt-6">
+        <div className="w-full flex flex-col items-center justify-center gap-4 max-w-safe-viewport">
+          <div className="w-full flex items-center justify-center gap-3">
+          </div>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
