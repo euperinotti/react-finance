b27fe6a219357da96ce5477fd26b18d72c8237fd
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-20 12:51:49 -0300

    feat: add delete and update entry

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -2,9 +2,11 @@
 
 import {
   IWalletContext,
+  IWalletEntry,
   WalletOperation,
 } from "@/contexts/@types/WalletContextTypes";
 import { WalletProvider } from "@/contexts/WalletContext";
+import { randomUUID } from "crypto";
 import { useContext } from "react";
 
 export const useWallet = () => {
@@ -12,17 +14,50 @@ export const useWallet = () => {
     WalletProvider
   ) as IWalletContext;
 
-  const addEntry = (value: number, operation: WalletOperation) => {
+  const addEntry = (
+    value: number,
+    operation: WalletOperation,
+    description: string,
+    category: string
+  ) => {
     const newValue = wallet[operation] + value;
 
     setWallet({ ...wallet, [operation]: newValue });
-    setHistory([...history, { date: new Date(), operation, value }]);
+    setHistory([
+      ...history,
+      {
+        id: randomUUID(),
+        date: new Date(),
+        operation,
+        value,
+        description,
+        category,
+      },
+    ]);
 
     return { wallet, history };
   };
 
+  const deleteEntry = (entryId: string) => {
+    const newHistory = history.filter((entry) => entry.id !== entryId);
+    const newBalance = wallet.income - wallet.outcome;
+
+    setWallet({ ...wallet, balance: newBalance });
+    setHistory(newHistory);
+  };
+
+  const updateEntry = (entry: IWalletEntry) => {
+    const updatedHistory = history.map((entryItem) =>
+      entryItem.id === entry.id ? entry : entryItem
+    );
+
+    setHistory(updatedHistory);
+  };
+
   return {
     addEntry,
+    deleteEntry,
+    updateEntry,
     wallet,
     history,
   };
