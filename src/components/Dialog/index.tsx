43fd33753b1723d19eb2b0dcc7f6bf292d7c4745
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-21 13:26:24 -0300

    styles: dialog fill screen

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -15,7 +15,7 @@ export const Dialog = forwardRef(
     return (
       <dialog
         {...rest}
-        className="w-full min-h-dvh h-full z-50 absolute bg-blend-overlay bg-gray-900 bg-opacity-40 transition-opacity m-0 p-0"
+        className="min-w-full min-h-full h-dvh z-50 absolute bg-blend-overlay bg-gray-900 bg-opacity-40 transition-opacity m-0 p-0 right-0 "
         onClick={handleDialogClick}
         ref={ref}
       >
